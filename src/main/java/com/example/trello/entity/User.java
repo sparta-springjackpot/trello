588607dd97f386b6d406f43a1d172b2914674388
hjern <hjern.refrain@gmail.com>
2023-08-07 21:16:14 +0900

    1. init commit and create Entity

---
+++ src/main/java/com/example/trello/entity/User.java
@@ -0,0 +1,32 @@
+package com.example.trello.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class User extends Timestamped {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String username;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Column(nullable = false)
+	private String passwordConfirm;
+
+	@Column(nullable = false, unique = true)
+	private String nickname;
+
+}
