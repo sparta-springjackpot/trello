60f5e155d3612eea5aa1dfaf03033348429944f0
euna2000 <eunakoh2000@gmail.com>
2023-08-08 18:25:59 +0900

    컬럼 관련 서비스 기능

--- src/main/java/com/example/trello/service/ColumnsService.java
+++ src/main/java/com/example/trello/service/ColumnsService.java
@@ -1,9 +1,42 @@
 package com.example.trello.service;
 
+import com.example.trello.dto.ColumnRequestDto;
+import com.example.trello.dto.ColumnResponseDto;
+import com.example.trello.entity.Columns;
+import com.example.trello.repository.ColumnsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ColumnsService {
+
+    private final ColumnsRepository columnsRepository;
+
+    public Long createColumn(ColumnRequestDto requestDto) {
+        Columns column = new Columns();
+        column.setColumnName(requestDto.getColumnName());
+        column.setColumnNumber(requestDto.getColumnNumber());
+        return columnsRepository.save(column).getId();
+    }
+
+    public ColumnResponseDto editColumn(Long columnId, ColumnRequestDto requestDto) {
+        Columns column = columnsRepository.findById(columnId)
+                .orElseThrow(() -> new IllegalArgumentException("Column not found"));
+
+        column.setColumnName(requestDto.getColumnName());
+        column.setColumnNumber(requestDto.getColumnNumber());
+
+        Columns savedColumn = columnsRepository.save(column);
+        ColumnResponseDto responseDto = new ColumnResponseDto();
+        responseDto.setId(savedColumn.getId());
+        responseDto.setColumnName(savedColumn.getColumnName());
+        responseDto.setColumnNumber(savedColumn.getColumnNumber());
+
+        return responseDto;
+    }
+
+    public void deleteColumn(Long columnId) {
+        columnsRepository.deleteById(columnId);
+    }
 }
