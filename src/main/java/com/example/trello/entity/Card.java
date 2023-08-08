da6c1515fa5ba1b23710fa7b8f84a3bf6f3f418d
dilution0216 <hglad0216@naver.com>
2023-08-08 17:48:52 +0900

    <Entity> CardEntity 구현 ERD 에 따라 cardName,cardDescription,cardColor,cardNumber 구현, this 사용 get 구현

--- src/main/java/com/example/trello/entity/Card.java
+++ src/main/java/com/example/trello/entity/Card.java
@@ -1,5 +1,6 @@
 package com.example.trello.entity;
 
+import com.example.trello.dto.CardRequestDto;
 import jakarta.persistence.*;
 import jakarta.persistence.Column;
 import lombok.Getter;
@@ -16,5 +17,23 @@ public class Card {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "card_id")
     private Long id;
-}
 
+    @Column(nullable = false)
+    private String cardName;
+
+    @Column(nullable = false)
+    private String cardDescription;
+
+    @Column(nullable = false)
+    private String cardColor;
+
+    @Column(nullable = false)
+    private Long cardNumber;
+
+    public Card(CardRequestDto requestDto) {
+        this.cardName = requestDto.getCardName();
+        this.cardDescription = requestDto.getCardDescription();
+        this.cardColor = requestDto.getCardColor();
+        this.cardNumber = requestDto.getCardNumber();
+    }
+}
