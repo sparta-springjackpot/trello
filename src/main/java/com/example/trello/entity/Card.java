7dea86ce04ec24257f2030b52ea6180f73666a0c
dilution0216 <hglad0216@naver.com>
2023-08-10 13:50:00 +0900

    <Entity> CardEntity 수정 ( 테이블 매핑 등 )

--- src/main/java/com/example/trello/entity/Card.java
+++ src/main/java/com/example/trello/entity/Card.java
@@ -7,6 +7,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Setter
 @Getter
 @NoArgsConstructor
@@ -18,9 +21,19 @@ public class Card {
     @Column(name = "card_id")
     private Long id;
 
+    // 연관 관계 매핑
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="column_id")
+    private Columns columns;
+
+//    @OneToMany(mappedBy = "card", cascade = CascadeType.REMOVE, orphanRemoval = true)
+//    private List<Worker> workers = new ArrayList<>();
+
+    // title
     @Column(nullable = false)
     private String cardName;
 
+    // description
     @Column(nullable = false)
     private String cardDescription;
 
@@ -30,10 +43,24 @@ public class Card {
     @Column(nullable = false)
     private Long cardNumber;
 
-    public Card(CardRequestDto requestDto) {
+//    @OneToMany(mappedBy = "card", cascade = CascadeType.REMOVE, orphanRemoval = true)
+//    private List<Comment> commentList = new ArrayList<>();
+
+    public Card(Columns coulmns, CardRequestDto requestDto) {
         this.cardName = requestDto.getCardName();
         this.cardDescription = requestDto.getCardDescription();
-        this.cardColor = requestDto.getCardColor();
+//        this.cardColor = requestDto.getCardColor();
         this.cardNumber = requestDto.getCardNumber();
     }
+
+
+    public void update(CardRequestDto cardRequestDto) {
+        this.cardName = cardRequestDto.getCardName();
+        this.cardDescription = cardRequestDto.getCardDescription();
+    }
+
+//    public void moveCard(Columns columns) {
+//        this.columns = columns;
+//    }
+
 }
