075797bf6d8195945fec8b0abc77a642dfd1bcb0
dilution0216 <hglad0216@naver.com>
2023-08-11 16:17:17 +0900

    <Entity> Worker 생성

--- src/main/java/com/example/trello/entity/Worker.java
+++ src/main/java/com/example/trello/entity/Worker.java
@@ -1,29 +1,29 @@
-//package com.example.trello.entity;
-//
-//import jakarta.persistence.*;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import org.springframework.security.core.userdetails.User;
-//
-//@Getter
-//@Entity
-//@NoArgsConstructor
-//public class Worker {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "card_id")
-//    private Card card;
-//
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "user_id")
-//    private User user;
-//
-//    public Worker(Card card, User newWorker) {
-//        this.card = card;
-//        this.user = newWorker;
-//    }
-//}
+package com.example.trello.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Worker {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "card_id")
+    private Card card;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private com.example.trello.entity.User user;
+
+    public Worker(Card card, User newWorker) {
+        this.card = card;
+        this.user = newWorker;
+    }
+}
