18bb4b8e6c2cf90b3ef1709064917bf70e68f34e
dilution0216 <hglad0216@naver.com>
2023-08-11 16:17:38 +0900

    <Repository> PermissionRepo 오류 수정

--- src/main/java/com/example/trello/repository/PermissionRepository.java
+++ src/main/java/com/example/trello/repository/PermissionRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.security.Permission;
 import java.util.List;
 
+
 public interface PermissionRepository extends JpaRepository<Permission, Long> {
     boolean existsByBoardIdAndUserId(Long boardId, Long userId);
 
