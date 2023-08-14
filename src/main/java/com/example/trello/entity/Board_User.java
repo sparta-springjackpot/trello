8ee9a82e6c54dbd3c6d2661edd1844044e56c537
wlstj <redvoca100466@gmail.com>
2023-08-14 18:09:44 +0900

    M : N 관계를 위한 중간 테이블

--- src/main/java/com/example/trello/entity/Board_User.java
+++ src/main/java/com/example/trello/entity/Board_User.java
@@ -1,5 +1,6 @@
 package com.example.trello.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,9 +8,21 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
+@Table(name = "board_user")
 public class Board_User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
+    @Column(name = "board_user_id")
     private Long id;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id2")
+    private Board board;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id2")
+    private User user;
+
+
 }
