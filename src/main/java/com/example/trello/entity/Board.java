f0530a4af945ce82799aeeb078c5b848b94adc87
wlstj <redvoca100466@gmail.com>
2023-08-14 18:09:23 +0900

    보드 앤티티 추가

--- src/main/java/com/example/trello/entity/Board.java
+++ src/main/java/com/example/trello/entity/Board.java
@@ -1,10 +1,15 @@
 package com.example.trello.entity;
 
+import com.example.trello.dto.BoardRequestDto;
+import com.example.trello.dto.CardRequestDto;
 import jakarta.persistence.*;
 import jakarta.persistence.Column;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Setter
 @Getter
@@ -16,4 +21,30 @@ public class Board {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "board_id")
     private Long id;
+
+    @OneToMany(mappedBy = "board",cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Board_User> board_userList = new ArrayList<>();
+
+    @Column(nullable = false)
+    private String board_name;
+
+    @Column(nullable = false)
+    private String back_color;
+
+    @Column(nullable = false)
+    private String introduce;
+
+    public Board(BoardRequestDto boardRequestDto) {
+        this.board_name = boardRequestDto.getBoard_name();
+        this.back_color = boardRequestDto.getBack_color();
+        this.introduce= boardRequestDto.getIntroduce();
+    }
+
+
+    public void update(BoardRequestDto boardRequestDto) {
+        this.board_name = boardRequestDto.getBoard_name();
+        this.back_color = boardRequestDto.getBack_color();
+        this.introduce= boardRequestDto.getIntroduce();
+    }
+
 }
