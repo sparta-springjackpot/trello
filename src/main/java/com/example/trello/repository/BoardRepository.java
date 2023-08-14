b00cbd5127eae319bae5bdd07ef65589b2b8065c
wlstj <redvoca100466@gmail.com>
2023-08-14 18:10:02 +0900

    보드 레퍼지토리

--- src/main/java/com/example/trello/repository/BoardRepository.java
+++ src/main/java/com/example/trello/repository/BoardRepository.java
@@ -3,5 +3,7 @@ package com.example.trello.repository;
 import com.example.trello.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
 }
