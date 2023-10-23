ed4769aa002feeae64399cd3e17ace527abcfe0f
MO SUNGMIN <104157577+Garim12@users.noreply.github.com>
2023-10-23 19:49:39 +0900

    Update SchedulingConfig.java

--- src/main/java/com/example/trello/config/SchedulingConfig.java
+++ src/main/java/com/example/trello/config/SchedulingConfig.java
@@ -13,7 +13,7 @@ public class SchedulingConfig {
     @Autowired
     private TaskService taskService;
 
-    // 스케줄러로 0초 1분 *시 *일 *달 *년마다 자동 실행됨
+    // 스케줄러로 0초 1분 *시 *일 *주 *월마다 자동 실행됨
     @Scheduled(cron = "0 0/1 * * * *")
     public void processExpiredTasks() {
         //processExpiredTasks로 주입
